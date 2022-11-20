5cb5cb3253bde62c7efbbe36d9ce654f5cdc0340
Evgenii Kolesnikov <eugene-kolesnikov@hotmail.com>
2022-11-20 22:57:09 +0300

    Initial commit

---
+++ recipes/calculator/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, HttpResponse
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+
+}
+
+
+def recipe_view(request, recipe):
+    servings = None
+    if request.GET:
+        servings = int(request.GET.get('qnt'))
+    else:
+        servings = 1
+
+    data_for_context = dict()
+    if recipe in DATA.keys():
+        for item, value in DATA[recipe].items():
+            data_for_context[item] = round(value * servings, 2)
+
+        context = {
+            'recipe': data_for_context
+        }
+        return render(request, 'calculator/index.html', context=context)
+    else:
+        return HttpResponse('no recipe for the dish or incorrect name of the dish')
+
